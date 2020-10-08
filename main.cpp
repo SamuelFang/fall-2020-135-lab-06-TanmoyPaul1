d7141e0b52b65f6f85f1a8bb145c51160a74992d
TanmoyPaul1 <pault@bxscience.edu>
2020-10-08 15:23:17 -0400

    Added all files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
+  test_ascii("");
+  test_ascii(" ! @ # $ %");
+  test_ascii("ABCDEFGHI");
+  test_ascii("abcdefghi");
+  test_ascii("Cat :3 Dog");
   return 0;
 }
