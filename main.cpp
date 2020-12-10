d247db56a71d08f56f22944978369921d0401768
SamuelFang <47552568+SamuelFang@users.noreply.github.com>
2020-12-10 17:25:00 -0500

    last part of lab 6
    added extra print statements for better testing of decode

--- main.cpp
+++ main.cpp
@@ -6,28 +6,25 @@
 
 int main()
 {
-  std::cout << "test_ascii(""):" << std::endl;
   test_ascii("");
-  std::cout << "test_ascii(" ! @ # $ % "):" << std::endl;
   test_ascii(" ! @ # $ % ");
-  std::cout << "test_ascii("ABCDEFGHI"):" << std::endl;
   test_ascii("ABCDEFGHI");
-  std::cout << "test_ascii("abcdefghi"):" << std::endl;
   test_ascii("abcdefghi");
-  std::cout << "test_ascii("Cat :3 Dog"):" << std::endl;
   test_ascii("Cat :3 Dog");
-  std::cout << "decodeCaesar("Validation"):" << std::endl;
+  std::cout << std::endl;
+  std::cout << "decodeCaesar(\"Validation\"):" << std::endl;
   std::cout << decodeCaesar("Validation") << std::endl;
-  std::cout << "decodeCaesar("Afqnifynts"):" << std::endl;
+  std::cout << "decodeCaesar(\"Afqnifynts\"):" << std::endl;
   std::cout << decodeCaesar("Afqnifynts") << std::endl;
-  std::cout << "decodeCaesar("Hello"):" << std::endl;
+  std::cout << "decodeCaesar(\"Hello\"):" << std::endl;
   std::cout << decodeCaesar("Hello") << std::endl;
-  std::cout << "decodeCaesar("Ifmmp"):" << std::endl;
+  std::cout << "decodeCaesar(\"Ifmmp\"):" << std::endl;
   std::cout << decodeCaesar("Ifmmp") << std::endl;
-  std::cout << "decodeCaesar("abcdef"):" << std::endl;
+  std::cout << "decodeCaesar(\"abcdef\"):" << std::endl;
   std::cout << decodeCaesar("abcdef") << std::endl;
-  std::cout << "decodeCaesar("bcdefg"):" << std::endl;
+  std::cout << "decodeCaesar(\"bcdefg\"):" << std::endl;
   std::cout << decodeCaesar("bcdefg") << std::endl;
+  std::cout << std::endl;
   // std::cout << encryptCaesar("Validation", 5) << std::endl;
   // std::cout << encryptCaesar("Phone", 3) << std::endl;
   return 0;
